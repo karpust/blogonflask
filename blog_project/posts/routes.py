25312d3394c93f889b494461440130d9cce30f1f
Nchameleo <956277@mail.ru>
2023-04-02 14:57:14 +0300

    lesson 3: step 4 implemented detail, update, delete post

--- blog_project/posts/routes.py
+++ blog_project/posts/routes.py
@@ -1,4 +1,5 @@
-from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask import Blueprint, request, render_template, flash, redirect, \
+    url_for, abort
 from flask_login import login_required, current_user
 from blog_project.models import Post
 from blog_project.posts.forms import PostForm
@@ -27,4 +28,46 @@ def create_post():
         flash('Ваш пост создан', 'success')
         return redirect(url_for('posts.allposts'))  # достает урл из вьюшки: redirect('/allposts')
     return render_template('create_post.html', title='Новый пост', form=form, legend='Новый пост')
+
+
+@posts.route('/post/<int:post_id>', methods=['GET'])
+@login_required
+def detail_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    return render_template('detail_post.html', title=post.title, post=post)
+
+
+@posts.route('/post/<int:post_id>/update', methods=['GET', 'POST'])
+@login_required
+def update_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    form = PostForm()
+    if form.validate_on_submit():
+        post.title = form.title.data
+        post.content = form.content.data
+        db.session.commit()
+        flash('Ваш пост обновлен', 'success')
+        return redirect(url_for('posts.detail_post', post_id=post_id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+        return render_template('create_post.html', form=form, title='Обновление поста',
+                               legend='Обновление поста')
+
+
+@posts.route('/post/<int:post_id>/delete', methods=['POST'])
+@login_required
+def delete_post(post_id):
+    post = Post.query.get_or_404(post_id)
+    if post.author != current_user:
+        abort(403)
+    db.session.delete(post)
+    db.session.commit()
+    flash('Ваш пост был удален!', 'success')
+    return redirect(url_for('posts.allposts'))
+
+
+
 
