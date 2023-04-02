35afdc77d37eafafea6b67bc9f2996139d7633a6
Nchameleo <956277@mail.ru>
2023-04-02 14:02:03 +0300

    lesson 3: step 3 create post

--- blog_project/posts/routes.py
+++ blog_project/posts/routes.py
@@ -1,6 +1,8 @@
-from flask import Blueprint, request, render_template
-from flask_login import login_required
+from flask import Blueprint, request, render_template, flash, redirect, url_for
+from flask_login import login_required, current_user
 from blog_project.models import Post
+from blog_project.posts.forms import PostForm
+from blog_project import db
 
 
 posts = Blueprint('posts', __name__)  # план
@@ -14,4 +16,15 @@ def allposts():
     return render_template('allposts.html', posts=posts)  # именован параметр posts= будет исп в шаблоне
 
 
+@posts.route('/post/new', methods=['GET', 'POST'])
+@login_required
+def create_post():
+    form = PostForm()
+    if form.validate_on_submit():
+        post = Post(title=form.title.data, content=form.content.data, author=current_user)
+        db.session.add(post)
+        db.session.commit()
+        flash('Ваш пост создан', 'success')
+        return redirect(url_for('posts.allposts'))  # достает урл из вьюшки: redirect('/allposts')
+    return render_template('create_post.html', title='Новый пост', form=form, legend='Новый пост')
 
